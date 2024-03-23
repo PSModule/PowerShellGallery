7d85f78973b44d15021e169ee3f46e7533551bd0
Marius Storhaug <marstor@hotmail.com>
2024-03-23 15:42:45 +0100

    🩹 [Patch]: Added placeholder functions (#3)
    ## Description
    
    - Added placeholder functions
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- src/PowerShellGallery/public/Get-PSGalleryAPI.ps1
+++ src/PowerShellGallery/public/Get-PSGalleryAPI.ps1
@@ -6,5 +6,5 @@
     [CmdletBinding()]
     param()
 
-    Invoke-RestMethod -Method Get -Uri https://www.powershellgallery.com/api/v2/ -ContentType 'application/json'
+    Invoke-RestMethod -Method Get -Uri 'https://www.powershellgallery.com/api/v2/' -ContentType 'application/json'
 }
---
+++ src/PowerShellGallery/public/Get-PSGalleryResource.ps1
@@ -0,0 +1,34 @@
+﻿function Get-PSGalleryResource {
+    <#
+    .SYNOPSIS
+    Get a resource from the PowerShell Gallery.
+
+    .DESCRIPTION
+    Long description
+
+    .EXAMPLE
+    Get-PSGalleryResource -Name 'MyModule' -Version '1.0.0'
+
+    .NOTES
+    General notes
+    #>
+
+    [CmdletBinding()]
+    param (
+        # Name of the module.
+        [Parameter(Mandatory)]
+        [string] $Name,
+
+        # The API key for the PowerShell Gallery.
+        [Parameter(Mandatory)]
+        [string] $APIKey
+    )
+
+    $uri = "https://www.powershellgallery.com/packages/$Name"
+
+    $body = @{
+        __RequestVerificationToken = $APIKey
+    }
+
+    Invoke-RestMethod -Uri $uri -Method Get -Body $body
+}
