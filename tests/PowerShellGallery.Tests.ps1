01ea7c2451b620be4c84a6ccb56dcd2cdfd89fdc
Marius Storhaug <marstor@hotmail.com>
2024-03-28 22:13:05 +0100

    align

---
+++ tests/PowerShellGallery.Tests.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Describe 'PowerShellGallery' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'PowerShellGallery' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'PowerShellGallery' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be importable' {
+            { Import-Module -Name 'PowerShellGallery' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
+        }
+    }
+}
