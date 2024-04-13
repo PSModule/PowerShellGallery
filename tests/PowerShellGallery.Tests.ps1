a2a2604f198622fc35f17664366d286b42ade40f
Marius Storhaug <marstor@hotmail.com>
2024-04-14 00:45:08 +0200

    Remove import module test

--- tests/PowerShellGallery.Tests.ps1
+++ tests/PowerShellGallery.Tests.ps1
@@ -8,13 +8,4 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'PowerShellGallery' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'PowerShellGallery' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'PowerShellGallery' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be importable' {
-            { Import-Module -Name 'PowerShellGallery' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
 }
