84b53579dc8632f1b9772e84dbad81689f9bfd28
Marius Storhaug <marstor@hotmail.com>
2024-10-07 10:23:28 +0200

    🚀 [Feature]: Restructure for Process-PSModule v3 (#13)
    ## Description
    
    - Removing support for Windows PowerShell - directional change for
    PSModule.
    - Remove nuget from Dependabot settings
    - Using Process-PSModule v3
    - Restructure for v3
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/PowerShellGallery.Tests.ps1
+++ tests/PowerShellGallery.Tests.ps1
@@ -8,4 +8,19 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'PowerShellGallery' {
+    Context 'Function: Get-PSGalleryAPI' {
+        It 'Should not throw' {
+            { Get-PSGalleryAPI } | Should -Not -Throw
+        }
+    }
+    Context 'Function: Hide-PowerShellGalleryItem' {
+        It 'Should not throw' {
+            { Hide-PowerShellGalleryItem } | Should -Not -Throw
+        }
+    }
+    Context 'Function: Show-PowerShellGalleryItem' {
+        It 'Should not throw' {
+            { Show-PowerShellGalleryItem } | Should -Not -Throw
+        }
+    }
 }
