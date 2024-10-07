655a76d17c002cca06110e7cdec6a2b82ecc08b6
Marius Storhaug <marstor@hotmail.com>
2024-10-07 09:58:30 +0200

    Added tests for existing public functions

--- tests/PowerShellGallery.Tests.ps1
+++ tests/PowerShellGallery.Tests.ps1
@@ -8,4 +8,24 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'PowerShellGallery' {
+    Context 'Function: Get-PSGalleryAPI' {
+        It 'Should not throw' {
+            { Get-PSGalleryAPI } | Should -Not -Throw
+        }
+    }
+    Context 'Function: Get-PSGalleryResource' {
+        It 'Should not throw' {
+            { Get-PSGalleryResource } | Should -Not -Throw
+        }
+    }
+    Context 'Function: Hide-PowerShellGalleryItem' {
+        It 'Should not throw' {
+            { Hide-PowerShellGalleryItem } | Should -Not -Throw
+        }
+    }
+    Context 'Function: Show-PowerShellGalleryItem' {
+        It 'Should not throw' {
+            { Show-PowerShellGalleryItem } | Should -Not -Throw
+        }
+    }
 }
